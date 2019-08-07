397c374a2517eca859a678fc6a9d0669b15bd6da
AlessandroPastore81 <53182723+AlessandroPastore81@users.noreply.github.com>
2019-08-07 11:46:56 +0100

    Add files via upload

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@
 #Importing libraries
 from matplotlib import pyplot as plt
 import numpy as np
-from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.backends.backend_pdf
 
 
 # In[11]:
@@ -69,8 +69,10 @@ EigN = np.loadtxt('neutron_singleparticles.dat')
 EigP = np.loadtxt('proton_singleparticles.dat')
 fig,ax = plt.subplots(1,2)
 ax[0].hlines(EigN[:,0],0,1)
+ax[0].set_title('Neutron states')
 plt.setp(ax[0].get_xticklabels(), visible=False)
 ax[1].hlines(EigP[:,0],0,1)
+ax[1].set_title('Proton states')
 plt.setp(ax[1].get_xticklabels(), visible=False)
 plt.savefig('eigen.pdf')
 
@@ -80,12 +82,68 @@ Wfn = np.loadtxt('neutron_wfs.dat')
 Wfp = np.loadtxt('protons_wfs.dat')
 Wfn2=Wfn[:,1].reshape(int(len(Wfn[:,1])/Npoints),Npoints)
 Wfp2=Wfp[:,1].reshape(int(len(Wfp[:,1])/Npoints),Npoints)
-fig,ax = plt.subplots(6)
-for i in range(6):
-    ax[i].plot(r,Wfn2[i,:])
-    ax[i].set(ylabel='n=$i')
-plt.savefig('wfneutron.pdf')
-fig,ax = plt.subplots(10)
-for i in range(6):
-    ax[i].plot(r,Wfp2[i,:])
-plt.savefig('wfproton.pdf')
+fig,ax = plt.subplots(3)
+c=0
+for i in range(3):
+	ax[i].plot(r,Wfn2[i,:])
+	if (EigN[i,1]==0 and c==0):
+		c=1
+		lab1='1s'+str(int(EigN[i,2]))+'/2'
+	elif (EigN[i,1]==1):
+		lab1='1p'+str(int(EigN[i,2]))+'/2'
+	elif (EigN[i,1]==2):
+		lab1='1d'+str(int(EigN[i,2]))+'/2'
+	else:
+		lab1='2s'+str(int(EigN[i,2]))+'/2'
+	ax[i].set(ylabel=lab1)
+plt.savefig('wfneutron1.pdf')
+fig,ax = plt.subplots(3)
+for i in range(3):
+	j=i+3
+	ax[i].plot(r,Wfn2[j,:])
+	if (EigN[j,1]==0 and c==0):
+		c=1
+		lab1='1s'+str(int(EigN[j,2]))+'/2'
+	elif (EigN[j,1]==1):
+		lab1='1p'+str(int(EigN[j,2]))+'/2'
+	elif (EigN[j,1]==2):
+		lab1='1d'+str(int(EigN[j,2]))+'/2'
+	else:
+		lab1='2s'+str(int(EigN[j,2]))+'/2'
+	ax[i].set(ylabel=lab1)
+plt.savefig('wfneutron2.pdf')
+fig,ax = plt.subplots(3)
+c=0
+for i in range(3):
+	ax[i].plot(r,Wfp2[i,:])
+	if (EigP[i,1]==0 and c==0):
+		c=1
+		lab1='1s'+str(int(EigP[i,2]))+'/2'
+	elif (EigP[i,1]==1):
+		lab1='1p'+str(int(EigP[i,2]))+'/2'
+	elif (EigP[i,1]==2):
+		lab1='1d'+str(int(EigP[i,2]))+'/2'
+	else:
+		lab1='2s'+str(int(EigP[i,2]))+'/2'
+	ax[i].set(ylabel=lab1)
+plt.savefig('wfproton1.pdf')
+fig,ax = plt.subplots(3)
+for i in range(3):
+	j=i+3
+	ax[i].plot(r,Wfp2[j,:])
+	if (EigP[j,1]==0 and c==0):
+		c=1
+		lab1='1s'+str(int(EigP[j,2]))+'/2'
+	elif (EigP[j,1]==1):
+		lab1='1p'+str(int(EigP[j,2]))+'/2'
+	elif (EigP[j,1]==2):
+		lab1='1d'+str(int(EigP[j,2]))+'/2'
+	else:
+		lab1='2s'+str(int(EigP[j,2]))+'/2'
+	ax[i].set(ylabel=lab1)
+plt.savefig('wfproton2.pdf')
+
+pdf = matplotlib.backends.backend_pdf.PdfPages('all.pdf')
+for fig in range(1, plt.gcf().number+1): ## will open an empty extra figure :(
+	pdf.savefig( fig )
+pdf.close()
